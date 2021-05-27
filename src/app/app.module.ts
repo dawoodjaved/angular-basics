9854446a61ec032a9fc6287093848dfaee593873
Dawoodjaveed <54910044+Dawoodjaveed@users.noreply.github.com>
2021-05-27 21:55:19 +0500

    second phase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { AddItemsComponent } from './components/add-items/add-items.component'
+import { ShowItemsComponent } from './components/show-items/show-items.component';
+import { RouterModule, Routes } from '@angular/router'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AddItemsComponent,
+    ShowItemsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/show-items/show-items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-items',
+  templateUrl: './show-items.component.html',
+  styleUrls: ['./show-items.component.css']
+})
+export class ShowItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
